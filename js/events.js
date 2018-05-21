30adc987ff5f3ea30b05e74495d37efd470bdde0
Trevor Russell <truss.42@gmail.com>
2018-05-21 01:21:08 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 $("p").on('click', getIt);
 $("input").on('keypress', function(key){
   console.log(key.keyCode);
-  if (key.keyCode == 71 || 103){
+  if (key.keyCode == 103){
     pressIt();
   }
 });

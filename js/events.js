5eb81979bc3645d62e55ed1a07f69bfc16225156
Trevor Russell <truss.42@gmail.com>
2018-05-21 00:58:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
 //define functions here
-
+function getIt(){
+  alert('Hey!');
+}
+function frameIt() {
+  $('img').addClass("tasty");
+}
+function pressIt() {
+  alert('G was pushed');
+}
+function submitIt() {
+  alert("Your form is going to be submitted now.");
+}
 $(document).ready(function(){
 
-// call functions here
-
+$("p").on('click', getIt);
+$("input").on('keypress', function(key){
+  console.log(key.keyCode);
+  if (key.keyCode == 71){
+    pressIt();
+  }
+});
+$('img').on('load', frameIt);
+$('form').on('submit', submitIt);
 });
